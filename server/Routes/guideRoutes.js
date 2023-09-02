73245e69a3f700c319910d2aad382d131fe2dc95
Emmanuel Rokah <36491225+MiniManch@users.noreply.github.com>
2023-09-03 00:03:52 +0300

    Update guideRoutes.js
    Added preview route for each of the guides

--- server/Routes/guideRoutes.js
+++ server/Routes/guideRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Guide = require('../Schemas/guideModel');
+const Lesson = require('../Schemas/lessonModel'); // Import your Lesson model
+
 
 // Route to get all guides
 router.get('/all', async (req, res) => {
@@ -9,6 +11,28 @@ router.get('/all', async (req, res) => {
     res.status(200).json(guides);
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while fetching guides.' });
+  }
+});
+
+// Get preview of a guide
+router.get('/preview/:guidename', async (req, res) => {
+  try {
+    const guideName = req.params.guidename;
+    const guide = await Guide.findOne({ name: guideName }).populate('lessons');
+
+    if (!guide) {
+      return res.status(404).json({ message: 'Guide not found' });
+    }
+
+    if (guide.lessons.length === 0) {
+      return res.status(404).json({ message: 'No lessons found for this guide' });
+    }
+
+    const firstLesson = guide.lessons[0]; // Get the first lesson
+    res.json(firstLesson);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
